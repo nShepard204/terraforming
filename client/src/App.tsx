afe75f2b31799b7083e5dbc8ee28780a80808088
Nic Shepard <71411118+nShepard204@users.noreply.github.com>
2026-08-28 12:34:38 -0400

    Basic UI done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,21 @@ import "./App.css";
 import axios from "axios";
 import { useState } from "react";
 
+const distanceSelectors = [10, 50, 100, 150, 200, 250, 300];
+
 function App() {
   const [data, setData] = useState("bleh");
+  const [userAddress, setUserAddress] = useState("");
+  const [userDistance, setUserDistance] = useState(0);
 
-  function handleFetchVenues() {
-    const addr = "2299 Waters Edge Blvd, Columbus, OH 43209";
+  function handleFetchVenues(addr: string, dist: number) {
     const requestUrl = `${import.meta.env.VITE_BACKEND_URL}/venues`;
 
     axios
       .get(requestUrl, {
         params: {
           address: addr,
-          distance: 175,
+          distance: dist,
         },
       })
       .then((data) => {
@@ -24,7 +27,26 @@ function App() {
 
   return (
     <>
-      <button onClick={() => handleFetchVenues()}>Click Me!</button>
+      <label id="user-address">Address: </label>
+      <input
+        id="user-address"
+        type="text"
+        value={userAddress}
+        onChange={(e) => setUserAddress(e.target.value)}
+      />
+      <br />
+      <label id="user-distance">Select Distance: </label>
+      <select
+        value={userDistance}
+        onChange={(e) => setUserDistance(parseInt(e.target.value))}
+      >
+        {distanceSelectors.map((distance) => (
+          <option value={distance}>{distance}</option>
+        ))}
+      </select>
+      <button onClick={() => handleFetchVenues(userAddress, userDistance)}>
+        Click Me!
+      </button>
       <br />
       {data}
     </>
