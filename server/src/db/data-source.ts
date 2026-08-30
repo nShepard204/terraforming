475f3a199a876acecd05830d9bef66e140d3d740
Nic Shepard <71411118+nShepard204@users.noreply.github.com>
2026-08-30 17:45:58 -0400

    It wants a pool for the driver instead?

--- server/src/db/data-source.ts
+++ server/src/db/data-source.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-import { Client } from 'pg';
+import { Pool } from 'pg';
 import { DataSource } from 'typeorm';
 import { Event } from '../entities/event.js';
 import { Host } from '../entities/host.js';
@@ -12,5 +12,5 @@ export const AppDataSource = new DataSource({
   entities: [Event, Host, Venue],
   synchronize: false,
   logging: false,
-  driver: { Client },
+  driver: { Pool },
 });
