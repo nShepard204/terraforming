9c2cefb35d75869dd8d786b652d03181855f9b18
Nic Shepard <71411118+nShepard204@users.noreply.github.com>
2026-08-31 12:55:41 -0400

    Added more cors headers

--- server/src/app.ts
+++ server/src/app.ts
@@ -22,7 +22,11 @@ app.use(
       }
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Access-Control-Allow-Origin'],
+    allowedHeaders: [
+      'Access-Control-Allow-Origin',
+      'Content-Type',
+      'Authorization',
+    ],
     credentials: true,
     optionsSuccessStatus: 200,
   })
